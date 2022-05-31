16339d9492f4a3cdc281813b0e22a80e6a1021ef
belovroman90 <belov.roman.90@mail.ru>
2022-05-31 09:11:36 +0300

    Complete 5 tasks

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -3,14 +3,21 @@ import s from './Header.module.css'
 import {PATH} from "./RoutesComponent";
 import {NavLink} from "react-router-dom";
 
-
 function Header() {
     return (
-        <div className={s.menu}>
-            <NavLink className={({isActive}) => isActive ? s.active : s.a} to={PATH.PRE_JUNIOR}>pre-junior</NavLink>
-            <NavLink className={({isActive}) => isActive ? s.active : s.a} to={PATH.JUNIOR}>junior</NavLink>
-            <NavLink className={({isActive}) => isActive ? s.active : s.a} to={PATH.JUNIOR_PLUS}>junior+</NavLink>
-        </div>
+        <nav className={s.container}>
+            <div className={s.icon}></div>
+            <ul className={s.menu}>
+                <li className={s.linkItem}><NavLink className={({isActive}) => isActive ? s.active : s.a}
+                                                    to={PATH.PRE_JUNIOR}>Pre-junior</NavLink></li>
+                <li className={s.linkItem}><NavLink className={({isActive}) => isActive ? s.active : s.a}
+                                                    to={PATH.JUNIOR}>Junior</NavLink>
+                </li>
+                <li className={s.linkItem}><NavLink className={({isActive}) => isActive ? s.active : s.a}
+                                                    to={PATH.JUNIOR_PLUS}>Junior+</NavLink></li>
+            </ul>
+        </nav>
+
     )
 }
 
